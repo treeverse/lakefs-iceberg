d1d1d966bb3f17c101384eecfabefe5d57cb80e5
Lynn Rozen <lynn.rozen@treeverse.io>
2023-06-23 01:59:37 +0300

    Fixed bug: IOException when writing parquet files

--- src/main/java/io/lakefs/iceberg/LakeFSInputFile.java
+++ src/main/java/io/lakefs/iceberg/LakeFSInputFile.java
@@ -3,6 +3,9 @@ package io.lakefs.iceberg;
 import org.apache.iceberg.io.InputFile;
 import org.apache.iceberg.io.SeekableInputStream;
 
+import java.io.IOException;
+import static java.util.Objects.requireNonNull;
+
 public class LakeFSInputFile implements InputFile {
     private final InputFile wrapped;
 
@@ -23,11 +26,44 @@ public class LakeFSInputFile implements InputFile {
 
     @Override
     public SeekableInputStream newStream(){
-        return wrapped.newStream();
+        return new LakeFSInputStream(wrapped.newStream());
     }
 
     @Override
     public boolean exists(){
         return wrapped.exists();
+    }
+
+    private static class LakeFSInputStream extends SeekableInputStream {
+        private final SeekableInputStream wrapped;
+
+        public LakeFSInputStream(SeekableInputStream wrapped) {
+            this.wrapped = requireNonNull(wrapped, "wrapped is null");
+        }
+
+        @Override
+        public int read() throws IOException {
+            return wrapped.read();
+        }
+
+        @Override
+        public int read(byte[] b, int off, int len) throws IOException {
+            return wrapped.read(b, off, len);
+        }
+
+        @Override
+        public long getPos() throws IOException {
+            return wrapped.getPos();
+        }
+
+        @Override
+        public void seek(long newPos) throws IOException {
+            wrapped.seek(newPos);
+        }
+
+        @Override
+        public void close() throws IOException {
+            wrapped.close();
+        }
     }
 }
