b8fbc6485ae214ae46ce86664cae9d6f6f68ccc2
johnnyaug <yoni.augarten@treeverse.io>
2023-06-29 11:28:19 +0300

    add some doc

--- src/main/java/io/lakefs/iceberg/LakeFSPath.java
+++ src/main/java/io/lakefs/iceberg/LakeFSPath.java
@@ -2,11 +2,20 @@ package io.lakefs.iceberg;
 
 import org.apache.hadoop.fs.Path;
 
+/**
+ * This is a Hadoop path with a dedicated {@link #toString()} implementation for lakeFS,
+ * which returns the path relative to the repository root.
+ * This Hadoop path is assumed to be a lakeFS path of the form <code>[scheme]://[repo-name]/[ref-name]/rest/of/path/</code>.
+ */
 public class LakeFSPath extends Path {
     public LakeFSPath(String pathString) throws IllegalArgumentException {
         super(pathString);
     }
 
+    /**
+     * Return the path relative to the lakeFS repository root.
+     * For example, given <code>s3a://example-repo/main/a/b/c</code>, return <code>a/b/c</code>.
+     */
     @Override
     public String toString() {
         return Util.GetPathFromURL(super.toString());
--- src/main/java/io/lakefs/iceberg/Util.java
+++ src/main/java/io/lakefs/iceberg/Util.java
@@ -6,6 +6,10 @@ public class Util {
 
     private Util() {}
 
+    /**
+     * Given a full lakeFS URI, return the path relative to the repository root.
+     * For example, given <code>s3a://example-repo/main/a/b/c</code>, return <code>a/b/c</code>.
+     */
     public static String GetPathFromURL(String lakeFSLocation){
         // return sub string after lakeFS ref
         return StringUtils.substring(lakeFSLocation, StringUtils.ordinalIndexOf(lakeFSLocation, "/", 4) + 1);
